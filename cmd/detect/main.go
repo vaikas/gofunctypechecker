37a0f4f60d1075ed00d393594bbad0c90d6261ef
Ville Aikas <vaikas@vmware.com>
2020-11-18 16:52:10 -0800

    playing around

--- cmd/detect/main.go
+++ cmd/detect/main.go
@@ -1,20 +1,81 @@
 package main
 
 import (
+	"fmt"
+	"github.com/vaikas/buildpackstuff/pkg/detect"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-
-	"github.com/vaikas/buildpackstuff/pkg/detect"
 )
 
+const plan = `
+[[provides]]
+name = "go-dist"
+
+[[provides]]
+name = "go-mod-vendor"
+
+[[provides]]
+name = "go-build"
+
+[[requires]]
+name = "go-dist"
+version = "gcr.io/paketo-buildpacks/go-dist:0.2.5"
+
+[[requires]]
+name = "go-mod-vendor"
+version = "gcr.io/paketo-buildpacks/go-mod-vendor:0.0.169"
+
+[[requires]]
+name = "go-build"
+version = "gcr.io/paketo-buildpacks/go-build:0.1.1"
+`
+
+const supportedFuncs = `
+import (
+    "context"
+	event "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/protocol"
+)
+
+The following function signatures are supported by this builder:
+func(event.Event)
+func(event.Event) protocol.Result
+func(context.Context, event.Event)
+func(context.Context, event.Event) protocol.Result
+func(event.Event) *event.Event
+func(event.Event) (*event.Event, protocol.Result)
+func(context.Context, event.Event) *event.Event
+func(context.Context, event.Event) (*event.Event, protocol.Result)
+`
+
+func printSupportedFunctionsAndExit() {
+	fmt.Println(supportedFuncs)
+	os.Exit(100)
+}
+
 func main() {
+	log.Println("ARGS: ", os.Args)
+	for _, e := range os.Environ() {
+		log.Println(e)
+	}
+
+	planFile, err := os.OpenFile(os.Args[2], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("failed to open the plan file for writing", os.Args[2], err)
+		printSupportedFunctionsAndExit()
+	}
+	defer planFile.Close()
+	if _, err := planFile.WriteString(string(plan)); err != nil {
+		printSupportedFunctionsAndExit()
+	}
+
 	// read all go files
-	files, err := filepath.Glob("./testdata/*.go")
+	files, err := filepath.Glob("./*.go")
 	if err != nil {
 		log.Printf("failed to read directory %s\n", err)
-		return
+		printSupportedFunctionsAndExit()
 	}
 
 	for _, f := range files {
@@ -23,7 +84,7 @@ func main() {
 		file, err := os.Open(f)
 		if err != nil {
 			log.Println(err)
-			return
+			printSupportedFunctionsAndExit()
 		}
 		defer file.Close()
 
@@ -31,11 +92,16 @@ func main() {
 		srcbuf, err := ioutil.ReadAll(file)
 		if err != nil {
 			log.Println(err)
-			return
+			printSupportedFunctionsAndExit()
 		}
 		f := &detect.Function{File: f, Source: string(srcbuf)}
 		if deets := detect.CheckFile(f); deets != nil {
 			log.Printf("Found supported function in package %q signature %q", deets.Package, deets.Signature)
+			os.Exit(0)
+			if err := os.Mkdir("TESTDIR", os.ModeDir); err != nil {
+				log.Println("FAILED TO MKDIR: ", err)
+			}
 		}
 	}
+	printSupportedFunctionsAndExit()
 }
